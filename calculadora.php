bdb61015d7f7e56695f279e1fcdb18f656d698dd
meliza021 <189984449+meliza021@users.noreply.github.com>
2025-07-08 13:56:55 +0000

    feat: :sparkles: SE ESTA TERMINANDO EL CICLO

--- calculadora.php
+++ calculadora.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora</title>
 </head>
+
 <body>
     <?php
-    $num1 =10;
+    $num1 = 10;
     $num2 = "10";
-?>
+    ?>
 
     <h1>Calculadora PHP</h1>
     <p>sumar: <?php echo $num1 + $num2 ?></p>
@@ -22,49 +24,46 @@
 
     <h1>comparacion PHP</h1>
     <P>resultados == <?php
-        if($num1 == $num2){
-            echo "son iguales";
-        }else
-        {
-            echo"no son iguales";
-        }
-    ?>
+                        if ($num1 == $num2) {
+                            echo "son iguales";
+                        } else {
+                            echo "no son iguales";
+                        }
+                        ?>
     </P>
     <P>resultados =! <?php
-        if($num1 != $num2){
-            echo "son diferentes";
-        }else
-        {
-            echo"no son diferentes";
-        }
-    ?>
+                        if ($num1 != $num2) {
+                            echo "son diferentes";
+                        } else {
+                            echo "no son diferentes";
+                        }
+                        ?>
     </P>
 
     <P>resultados >= : <?php
-        if($num1 >= $num2){
-            echo "es mayor o igual";
-        }else
-        {
-            echo"no es mayor";
-        }
-    ?>
+                        if ($num1 >= $num2) {
+                            echo "es mayor o igual";
+                        } else {
+                            echo "no es mayor";
+                        }
+                        ?>
     </P>
     <P>resultados <= : <?php
-        if($num1 >= $num2){
-            echo "es menor o igual";
-        }else
-        {
-            echo"no es menor";
-        }
-    ?>
-    </P>
-    <P>resultados <=> : <?php
-        echo" 5 -> 5 ". (5 <=> 5) ."<br>";
-        echo" 5 -> 4 " . (5 <=> 4)."<br>";
-        echo"-5 -> 6 ". (5 <=> 6) ."<br>";
+                        if ($num1 >= $num2) {
+                            echo "es menor o igual";
+                        } else {
+                            echo "no es menor";
+                        }
+                        ?>
+            </P>
+            <P>resultados <=> : <?php
+                                echo " 5 -> 5 " . (5 <=> 5) . "<br>";
+                                echo " 5 -> 4 " . (5 <=> 4) . "<br>";
+                                echo "-5 -> 6 " . (5 <=> 6) . "<br>";
 
-    ?>
-    </P>
+                                ?>
+            </P>
 
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,49 +1,52 @@
-<?php
+D<?php
     $nombre = "Meliza";
     $apellido = "Ardila";
-    $skill = ["Git","Php","MySQL"];
+    $skill = ["Git", "Php", "MySQL"];
     $contraseña_usuario = "admin";
     $es_valida = false;
     $edad = 26;
     $nivel_pin_pon = 1.5;
-    define("EDAD_MINIMA",18);
+    define("EDAD_MINIMA", 18);
     const EDAD_MAXIMA = 99;
 
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to php</title>
 </head>
+
 <body>
     <h1>
-        <?php 
-            echo "HELLO" . " " . $nombre . " " . $apellido . "!"; 
+        <?php
+        echo "HELLO" . " " . $nombre . " " . $apellido . "!";
 
         ?>
     </h1>
     <h2>Skill</h2>
     <ul>
-        <li><?php printf("Skill nivel 1: %s",$skill[0]);?></li>
-        <li><?php printf("Skill nivel 2: %s",$skill[1]);?></li>
-        <li><?php printf("Skill nivel 3: %s",$skill[2]);?></li>
+        <li><?php printf("Skill nivel 1: %s", $skill[0]); ?></li>
+        <li><?php printf("Skill nivel 2: %s", $skill[1]); ?></li>
+        <li><?php printf("Skill nivel 3: %s", $skill[2]); ?></li>
     </ul>
     <h2>Requirements</h2>
-        <li><?php printf("Edad %s y %s", EDAD_MINIMA,EDAD_MAXIMA);?></li>
+    <li><?php printf("Edad %s y %s", EDAD_MINIMA, EDAD_MAXIMA); ?></li>
     <ul>
-    <h2>Debug</h2>
-    <ul>
-        <li><?php var_dump($nombre);?></li>
-        <li><?php var_dump($apellido);?></li>
-        <li><?php var_dump($skill);?></li>
-        <li><?php var_dump($contraseña_usuario);?></li>
-        <li><?php var_dump($es_valida);?></li>
-        <li><?php var_dump($edad);?></li>
-        <li><?php var_dump($nivel_pin_pon);?></li>
+        <h2>Debug</h2>
+        <ul>
+            <li><?php var_dump($nombre); ?></li>
+            <li><?php var_dump($apellido); ?></li>
+            <li><?php var_dump($skill); ?></li>
+            <li><?php var_dump($contraseña_usuario); ?></li>
+            <li><?php var_dump($es_valida); ?></li>
+            <li><?php var_dump($edad); ?></li>
+            <li><?php var_dump($nivel_pin_pon); ?></li>
 
-    </ul>
+        </ul>
 
 </body>
+
 </html>
