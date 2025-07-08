c986d6d232029fbeb494368d7314e42e231aea35
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-07-08 22:12:59 +0000

    feat: :sparkles: seguimos

---
+++ eejer.php
@@ -0,0 +1,21 @@
+<?php
+$num1 = 4;
+$num2 = 67;
+$resultado = $num1 + $num2;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>suma de dos numeros</title>
+</head>
+<body>
+        <p>
+            <?php
+                echo ("resultado:". $resultado )
+            ?>
+        </p>
+
+</body>
+</html>
